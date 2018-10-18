608f22f23143407b842567897095ce2e3fc0af84
Aaron Fisher <aaronfisher@MacBook-Pro-20.local>
2018-10-18 10:07:25 -0500

    Initial Commit

---
+++ triviaToWin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  triviaToWin
+//
+//  Created by Aaron Fisher on 10/18/18.
+//  Copyright © 2018 Aaron Fisher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
